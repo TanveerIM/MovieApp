d520283bbd10fdcd4a6ab2c94991f776396f6ccc
tanveer <tanveeratworks@gmail.com>
2024-02-01 21:22:50 +0530

    creating HomeScreen and searchInput header design.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,74 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  ActivityIndicator,
+  ScrollView,
+  StatusBar,
+} from 'react-native';
+import {COLORS, SPACING} from '../theme/theme';
+import {
+  upcomingMovies,
+  nowPlayingMovies,
+  popularMovies,
+  baseImagePath,
+} from '../api/apicalls';
+import InputHeader from '../components/InputHeader';
 
-const HomeScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>HomeScreen</Text>
-    </View>
-  );
+const {width, height} = Dimensions.get('window');
+
+const HomeScreen = ({navigation}: any) => {
+  const [nowPlayingMoviesList, setNowPlayingMoviesList] =
+    React.useState<any>(undefined);
+  const [popularMoviesList, setPopularMoviesList] =
+    React.useState<any>(undefined);
+  const [upcomingMoviesList, setUpcomingMoviesList] =
+    React.useState<any>(undefined);
+
+  if (
+    nowPlayingMoviesList == undefined &&
+    nowPlayingMoviesList == null &&
+    popularMoviesList == undefined &&
+    popularMoviesList == null &&
+    upcomingMoviesList == undefined &&
+    upcomingMoviesList == null
+  ) {
+    return <ScrollView style={styles.container} bounces={false}
+    contentContainerStyle={styles.scrollViewContainer}>
+      <StatusBar hidden />
+      
+      <View style={styles.InputHeaderContainer}>
+        <InputHeader />
+      </View>
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size={'large'} color={COLORS.Orange} />
+      </View>
+    </ScrollView>;
+  }
+
+  return <View style={styles.container}></View>;
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
-    
+    display: 'flex',
+    backgroundColor: COLORS.Black,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  InputHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
   }
 });
