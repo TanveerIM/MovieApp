bc5cfbcdfded7db690dd096eb504ccaf81ff71f7
tanveer <tanveeratworks@gmail.com>
2024-02-02 12:25:26 +0530

    creating cateogryHeader, inputHeader and subMovieCards.

--- src/components/InputHeader.tsx
+++ src/components/InputHeader.tsx
@@ -1,18 +1,40 @@
 import * as React from 'react';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
 import CustomIcon from './CustomIcon';
 
-const InputHeader = () => {
-    const [searchText, setSearchText] = React.useState<string>('');
+const InputHeader = (props: any) => {
+  const [searchText, setSearchText] = React.useState<string>('');
 
   return (
     <View style={styles.inputBox}>
-      <TextInput style={styles.textInput} onChangeText={ textInput => setSearchText(textInput)}
-      placeholder='Search your Movies...'
-      placeholderTextColor={COLORS.WhiteRGBA32} />
-      <TouchableOpacity style={styles.searchIcon}>
-        <CustomIcon name='search' color={COLORS.Orange} size={FONTSIZE.size_20}/>
+      <TextInput
+        style={styles.textInput}
+        onChangeText={textInput => setSearchText(textInput)}
+        value={searchText}
+        placeholder="Search your Movies..."
+        placeholderTextColor={COLORS.WhiteRGBA32}
+      />
+      <TouchableOpacity
+        style={styles.searchIcon}
+        onPress={() => props.searchFunction(searchText)}>
+        <CustomIcon
+          name="search"
+          color={COLORS.Orange}
+          size={FONTSIZE.size_20}
+        />
       </TouchableOpacity>
     </View>
   );
@@ -21,24 +43,24 @@ const InputHeader = () => {
 export default InputHeader;
 
 const styles = StyleSheet.create({
-    inputBox: {
-        display: 'flex',
-        paddingVertical: SPACING.space_8,
-        paddingHorizontal: SPACING.space_24,
-        borderWidth: 2,
-        borderColor: COLORS.WhiteRGBA15,
-        borderRadius: BORDERRADIUS.radius_25,
-        flexDirection: 'row',
-    },
-    textInput: {
-        width: '90%',
-        fontFamily: FONTFAMILY.poppins_regular,
-        fontSize: FONTSIZE.size_14,
-        color: COLORS.White,
-    },
-    searchIcon: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: SPACING.space_10,
-    }
+  inputBox: {
+    display: 'flex',
+    paddingVertical: SPACING.space_8,
+    paddingHorizontal: SPACING.space_24,
+    borderWidth: 2,
+    borderColor: COLORS.WhiteRGBA15,
+    borderRadius: BORDERRADIUS.radius_25,
+    flexDirection: 'row',
+  },
+  textInput: {
+    width: '90%',
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  searchIcon: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: SPACING.space_10,
+  },
 });
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -8,6 +8,7 @@ import {
   ActivityIndicator,
   ScrollView,
   StatusBar,
+  FlatList,
 } from 'react-native';
 import {COLORS, SPACING} from '../theme/theme';
 import {
@@ -17,8 +18,40 @@ import {
   baseImagePath,
 } from '../api/apicalls';
 import InputHeader from '../components/InputHeader';
+import CategoryHeader from '../components/CategoryHeader';
+import SubMovieCard from '../components/SubMovieCards';
 
 const {width, height} = Dimensions.get('window');
+
+const getNowPlayingMoviesList = async () => {
+  try {
+    let reponse = await fetch(nowPlayingMovies);
+    let json = await reponse.json();
+    return json
+  } catch(error) {
+    console.error('Something went wrong in getNowPlayingMoviesList Function', error)
+  }
+}
+
+const getUpcomingMoviesList = async () => {
+  try {
+    let response = await fetch(upcomingMovies);
+    let json = response.json();
+    return json
+  } catch(error) {
+    console.error('Something went wrong in getUpcomingMoviesList Function', error)
+  }
+}
+
+const getPopularMoviesList = async () => {
+  try {
+    let response = await fetch(popularMovies);
+    let json = response.json();
+    return json
+  } catch(error) {
+    console.error('Something  went wrong in getPopularMoviesList Function', error)
+  }
+}
 
 const HomeScreen = ({navigation}: any) => {
   const [nowPlayingMoviesList, setNowPlayingMoviesList] =
@@ -28,6 +61,25 @@ const HomeScreen = ({navigation}: any) => {
   const [upcomingMoviesList, setUpcomingMoviesList] =
     React.useState<any>(undefined);
 
+    React.useEffect(() => {
+      (async() => {
+        let tempNowPlaying = await getNowPlayingMoviesList();
+        setNowPlayingMoviesList(tempNowPlaying.results);
+
+        let tempPopular = await getPopularMoviesList();
+        setPopularMoviesList(tempPopular.results);
+
+        let tempUpcoming = await getUpcomingMoviesList();
+        setUpcomingMoviesList(tempUpcoming.results);
+
+      })();
+    }, []);
+
+    console.log(nowPlayingMoviesList.length)
+
+    const searchMoviesFunction = () => {
+      navigation.navigate('Search')
+    }
   if (
     nowPlayingMoviesList == undefined &&
     nowPlayingMoviesList == null &&
@@ -41,7 +93,7 @@ const HomeScreen = ({navigation}: any) => {
       <StatusBar hidden />
       
       <View style={styles.InputHeaderContainer}>
-        <InputHeader />
+        <InputHeader searchFunction={searchMoviesFunction} />
       </View>
       <View style={styles.loadingContainer}>
         <ActivityIndicator size={'large'} color={COLORS.Orange} />
@@ -49,7 +101,28 @@ const HomeScreen = ({navigation}: any) => {
     </ScrollView>;
   }
 
-  return <View style={styles.container}></View>;
+  return (
+    <ScrollView style={styles.container} bounces={false}
+    contentContainerStyle={styles.scrollViewContainer}>
+      <StatusBar hidden />
+      
+      <View style={styles.InputHeaderContainer}>
+        <InputHeader searchFunction={searchMoviesFunction} />
+      </View>
+
+      <CategoryHeader title={'Now Playing'} />
+      <CategoryHeader title={'Popular'} />
+      <CategoryHeader title={'Upcoming'} />
+      <FlatList
+      data={upcomingMoviesList}
+      keyExtractor={(item: any) => item.id}
+       horizontal
+       contentContainerStyle={styles.containerGap36}
+       renderItem={({item, index}) => (
+        <SubMovieCard title={item.original_title} />
+       )} />
+    </ScrollView>
+  );
 };
 
 export default HomeScreen;
@@ -70,5 +143,8 @@ const styles = StyleSheet.create({
   InputHeaderContainer: {
     marginHorizontal: SPACING.space_36,
     marginTop: SPACING.space_28,
+  },
+  containerGap36: {
+    gap: SPACING.space_36,
   }
 });
