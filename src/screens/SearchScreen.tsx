bb50d9f64330bae68faeb609db930945ff7ad46d
tanveer <tanveeratworks@gmail.com>
2024-02-07 13:36:58 +0530

    completing search screen UI design and also calling movies api call.

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,60 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  StatusBar,
+  FlatList,
+} from 'react-native';
+import {COLORS, SPACING} from '../theme/theme';
+import SubMovieCard from '../components/SubMovieCards';
+import {baseImagePath, searchMovies} from '../api/apicalls';
+import InputHeader from '../components/InputHeader';
 
-const SearchScreen = () => {
+const {width, height} = Dimensions.get('screen');
+
+const SearchScreen = ({navigation}: any) => {
+  const [searchList, setSearchList] = React.useState([]);
+
+  const searchMoviesFunction = async (name: string) => {
+    try {
+      let response = await fetch(searchMovies(name));
+      let json = await response.json();
+      setSearchList(json.results);
+    } catch (error) {
+      console.error('Something went wrong in searchMoviesFunction ');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>SearchScreen</Text>
+      <StatusBar hidden />
+      <FlatList
+        data={searchList}
+        keyExtractor={(item: any) => item.id}
+        bounces={false}
+        numColumns={2}
+        ListHeaderComponent={
+          <View style={styles.InputHeaderContainer}>
+            <InputHeader searchFunction={searchMoviesFunction} />
+          </View>
+        }
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.centreContainer}
+        renderItem={({item, index}) => (
+          <SubMovieCard
+            shouldMarginatedAtEnd={false}
+            shouldMarginatedAround={true}
+            cardFunction={() => {
+              navigation.push('MovieDetails', {movieid: item.id});
+            }}
+            cardWidth={width / 2 - SPACING.space_12*2}
+            title={item.original_title}
+            imagePath={baseImagePath('w342', item.poster_path)}
+          />
+        )}
+      />
     </View>
   );
 };
@@ -12,5 +62,20 @@ const SearchScreen = () => {
 export default SearchScreen;
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    display: 'flex',
+    flex: 1,
+    width,
+    alignItems: 'center',
+    backgroundColor: COLORS.Black,
+  },
+  InputHeaderContainer: {
+    display: 'flex',
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_28,
+    marginBottom: SPACING.space_28 - SPACING.space_12,
+  },
+  centreContainer: {
+    alignItems: 'center',
+  }
 });
